b3414006e110280ea4bd57463bbc2981b8d947dd
Ashfaque Alam <67138411+ashfaque@users.noreply.github.com>
2023-08-29 13:26:19 +0530

    Update dj_tasks_scheduler.py

--- dj_scheduler/management/commands/dj_tasks_scheduler.py
+++ dj_scheduler/management/commands/dj_tasks_scheduler.py
@@ -1,26 +1,33 @@
-# from users.tasks import test_task
-# test_task(repeat=5) # Repeat every 24 hours
-
+'''
+    Author: Ashfaque Alam
+    Date: August 29, 2023
+    Usage: python manage.py dj_tasks_scheduler
+'''
 
 from django.core.management.base import BaseCommand, CommandError
 from django.core.management import call_command
 
 class Command(BaseCommand):
+    '''
+        Usage: `python manage.py dj_tasks_scheduler`
+    '''
     help = 'Run a script and another management command'
 
     def handle(self, *args, **options):
-        # Your script logic here
         self.stdout.write(self.style.SUCCESS('Script executed'))
 
-        # Run another management command
         try:
+            # Deleting older registered tasks from the table `background_task`.
             from background_task.models import Task
             _ = Task.objects.all().delete()
-            # ! delete all older tasks here, also import tasks files from all apps here and run from here with, `python manage.py schedulers`
-            # ! better pick all the registered tasks from a file called, registered_tasks from main_app and run them here by importing.
+
+            # Registering new or updated tasks.
             from drf_signal_simplejwt.registered_tasks import registered_tasks
             registered_tasks()
-            call_command('process_tasks')    # This command (python manage.py process_tasks) will be executed after the script has finished.
+
+            # Executing (python manage.py process_tasks) to register the tasks in the database and run them.
+            call_command('process_tasks')
+
             self.stdout.write(self.style.SUCCESS('process_tasks command executed'))
         except CommandError as e:
             self.stdout.write(self.style.ERROR(f'Error executing process_tasks command: {e}'))
