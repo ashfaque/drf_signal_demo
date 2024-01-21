587688126ea127acc18324ac4bda9ef3b907d646
Ashfaque Alam <67138411+ashfaque@users.noreply.github.com>
2024-01-21 14:42:02 +0530

    updated server run script

--- script_run_server.py
+++ script_run_server.py
@@ -2,4 +2,4 @@ import os
 
 if __name__ == '__main__': 
     conda_env_name = 'senv'
-    os.system(f'C:\\Users\\%USERNAME%\\.conda\\envs\\{conda_env_name}\\python.exe -m uvicorn drf_signal_simplejwt.asgi:application --reload --host 0.0.0.0 --port 8000 --use-colors --log-level info')
+    os.system(f'C:\\Users\\%USERNAME%\\.conda\\envs\\{conda_env_name}\\python.exe -m uvicorn drf_signal_simplejwt.asgi:application --reload --host 0.0.0.0 --port 8000 --use-colors --log-level info --workers 1')
