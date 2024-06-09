b1ff3b746a12e17a9c984fe0ee2c7697a0d74704
Ashfaque Alam <67138411+ashfaque@users.noreply.github.com>
2024-06-09 14:18:51 +0530

    added cursor pagination and added condition to exclude super user in subscription check middleware

--- drf_signal_simplejwt/middleware.py
+++ drf_signal_simplejwt/middleware.py
@@ -50,29 +50,30 @@ def SubscriptionMiddleware(get_response):
                 print('User ID from AccessToken for SubscriptionMiddleware ----------> ', user)
 
                 user_obj = user_models.UserDetail.objects.get(id=user)
-                user_college_obj = user_obj.college if user_obj.college else None
-                college_plan_mapping_obj = CollegePlanMapping.objects.get(college=user_college_obj) if user_college_obj else None
-                if not college_plan_mapping_obj:
-                    response_data = {
-                        'error': f'College is not assigned to user with id: {user}.'
-                    }
-                    return JsonResponse(response_data, status=403)
-                # print('is_active::: ', not college_plan_mapping_obj.is_active)
-                # print('valid date expired::: ', college_plan_mapping_obj.plan_valid_until and (college_plan_mapping_obj.plan_valid_until.date() < date.today()))
-                # print('trial date expired::: ', college_plan_mapping_obj.is_trial and college_plan_mapping_obj.trial_end_date and (college_plan_mapping_obj.trial_end_date.date() < date.today()))
-                # print('is_cancelled::: ', college_plan_mapping_obj.is_cancelled)
-                # Check if the subscription has expired
-                if (
-                    not college_plan_mapping_obj.is_active                                                                                                                  # * not is_active
-                    or (college_plan_mapping_obj.plan_valid_until and college_plan_mapping_obj.plan_valid_until.date() < date.today())                                      # * or plan_valid_until
-                    or (college_plan_mapping_obj.is_trial and college_plan_mapping_obj.trial_end_date and college_plan_mapping_obj.trial_end_date.date() < date.today())    # * or is_trial & trial_end_date
-                    or college_plan_mapping_obj.is_cancelled                                                                                                                # * or is_cancelled
-                ):
-                    # Subscription has expired
-                    response_data = {
-                        'error': 'Subscription has expired. Please renew your subscription.'
-                    }
-                    return JsonResponse(response_data, status=403)
+                if not user_obj.is_superuser:    # If user is a superuser and request is coming from an API hit, with Knox token.
+                    user_college_obj = user_obj.college if user_obj.college else None
+                    college_plan_mapping_obj = CollegePlanMapping.objects.get(college=user_college_obj) if user_college_obj else None
+                    if not college_plan_mapping_obj:
+                        response_data = {
+                            'error': f'College is not assigned to user with id: {user}.'
+                        }
+                        return JsonResponse(response_data, status=403)
+                    # print('is_active::: ', not college_plan_mapping_obj.is_active)
+                    # print('valid date expired::: ', college_plan_mapping_obj.plan_valid_until and (college_plan_mapping_obj.plan_valid_until.date() < date.today()))
+                    # print('trial date expired::: ', college_plan_mapping_obj.is_trial and college_plan_mapping_obj.trial_end_date and (college_plan_mapping_obj.trial_end_date.date() < date.today()))
+                    # print('is_cancelled::: ', college_plan_mapping_obj.is_cancelled)
+                    # Check if the subscription has expired
+                    if (
+                        not college_plan_mapping_obj.is_active                                                                                                                  # * not is_active
+                        or (college_plan_mapping_obj.plan_valid_until and college_plan_mapping_obj.plan_valid_until.date() < date.today())                                      # * or plan_valid_until
+                        or (college_plan_mapping_obj.is_trial and college_plan_mapping_obj.trial_end_date and college_plan_mapping_obj.trial_end_date.date() < date.today())    # * or is_trial & trial_end_date
+                        or college_plan_mapping_obj.is_cancelled                                                                                                                # * or is_cancelled
+                    ):
+                        # Subscription has expired
+                        response_data = {
+                            'error': 'Subscription has expired. Please renew your subscription.'
+                        }
+                        return JsonResponse(response_data, status=403)
 
                 # except:
                 #     return JsonResponse({'error': 'Invalid JWT token'}, status=401)
@@ -205,4 +206,77 @@ class CaptureIPMiddleware:
 
         response = self.get_response(request)
         return response
+
+
+
+
+'''
+# middleware for knox token:-
+
+from django.http import JsonResponse
+from datetime import date
+from master.models import TMasterSubscriptionCompanyMapping
+from django.conf import settings
+from rest_framework.exceptions import APIException
+from knox.auth import TokenAuthentication
+
+
+def is_excluded_path(path: str, excluded_paths: list) -> bool:
+    import fnmatch
+    for pattern in excluded_paths:
+        if fnmatch.fnmatchcase(path, pattern):
+            return True
+    return False
+
+
+def CheckSubscriptionMiddleware(get_response):
+    def middleware(request):
+        # print('requst.path----------------> ', request.path)
+
+        if not is_excluded_path(request.path, settings.SUBSCRIPTION_EXCLUDED_URLS) and not request.user.is_superuser:    # ? Subscription will only be checked if the request is not for an excluded path. And user is not superuser.
+            # print(request.__dict__)
+            # print('request.user.is_authenticated---------------->', request.user.is_authenticated)
+            # print('user---------------->', request.user)
+            try:
+                user_obj, digest = TokenAuthentication().authenticate(request)    # Incorrect tokens will be handled here as well and a suitable error msg will also be thrown.
+            except Exception as ex:
+                return JsonResponse({'detail': str(ex)}, status=401)
+            if not user_obj.is_superuser:    # If user is a superuser and request is coming from an API hit, with Knox token.
+                user_company_obj = user_obj.cu_user.company if user_obj.cu_user.company else None
+                if not user_company_obj:
+                    return JsonResponse({
+                                'error': f'No company is assigned to user: {user_obj.username}.'
+                            }, status=403)
+
+                company_subscription_mapping_obj = TMasterSubscriptionCompanyMapping.objects.filter(company=user_company_obj).order_by('-created_at').first() if user_company_obj else None
+
+                if not company_subscription_mapping_obj:
+                    return JsonResponse({
+                                'error': f'No subscription registered for the company: {user_company_obj.coc_name}.'
+                            }, status=403)
+
+                # ? Check if the subscription has expired
+                subscription_valid_until = company_subscription_mapping_obj.grace_end_date if company_subscription_mapping_obj.grace_end_date else company_subscription_mapping_obj.subscription_valid_until
+                # If Subscription has expired.
+                if (
+                    not company_subscription_mapping_obj.is_active                                                                                                                  # * Subscription is not marked as active.
+                    or (company_subscription_mapping_obj.subscription_valid_from and company_subscription_mapping_obj.subscription_valid_from.date() > date.today())                # * Subscription not started yet.
+                    or (subscription_valid_until and subscription_valid_until.date() < date.today())                                                                                # * Subscription end data is exceeded or subscription grace period also exceeded.
+                    or (company_subscription_mapping_obj.is_trial and company_subscription_mapping_obj.trial_start_date and company_subscription_mapping_obj.trial_end_date and (   # * or is_trial=True & date.today() is NOT between trial_start_date or trial_end_date.
+                            company_subscription_mapping_obj.trial_start_date.date() > date.today() or company_subscription_mapping_obj.trial_end_date.date() < date.today())
+                    )
+                    or company_subscription_mapping_obj.is_cancelled                                                                                                                # * or is_cancelled
+                ):
+                    response_data = {
+                        'error': f'Subscription not active for the company: {user_company_obj.coc_name}.'
+                    }
+                    return JsonResponse(response_data, status=403)
+
+
+        # Allow the request to proceed.
+        response = get_response(request)
+        return response
+    return middleware
+
+'''
 
--- pagination.py
+++ pagination.py
@@ -1,4 +1,4 @@
-from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination
+from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination, CursorPagination
 from django.conf import settings
 
 class OnOffPagination(PageNumberPagination):
@@ -16,3 +16,32 @@ class OnOffPagination(PageNumberPagination):
             else:
                 pass
         return self.page_size
+
+
+'''
+Author: Ashfaque Alam
+Date: January 29, 2024
+Purpose: As PageNumberPagination or LimitOffsetPagination have their limitations on larger dataset in the database, this custom cursor based pagination class is created to overcome these limitations. Also, it works perfectly in infinite scrolling pagination in fontend.
+Usage: For the first time just send `page_size` with some integer value in query params, in the 'next' key of the response there will be a 'cursor' key which needs to be sent in query params in order to goto the subsequent next page. Or, just hit the 'next' page API URL in the response.
+Limitations: User will not be able to jump on random pages. They need to goto next or previous page sequentially.
+'''
+# from rest_framework.pagination import CursorPagination
+class OnOffCursorPagination(CursorPagination):
+    page_size = 10    # ? Default if no `page_size_query_param` supplied.
+    max_page_size = 1000
+    page_size_query_param = 'page_size'
+    ordering = '-id'    # Any model field will do but it should be unique.
+    cursor_query_param = 'cursor'
+
+    def get_page_size(self, request):
+        page_size_value_supplied_in_query_params = int(request.query_params.get(self.page_size_query_param, self.page_size))    # If none is supplied, default to `self.page_size`.
+        page_size = min(page_size_value_supplied_in_query_params, self.max_page_size)    # Which ever is smaller either `page_size_value_supplied_in_query_params` or `self.max_page_size`.
+
+        if page_size > 0:
+            return page_size
+        elif page_size == 0:
+            return None    # Return all data without pagination.
+        else:
+            raise ValueError('page_size query parameter should be supplied & its value needs to be greater than or equal to 0.')
+
+''' ENDs Ashfaque '''
